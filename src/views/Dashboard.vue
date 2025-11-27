0f284c79637c693fb421f8494dc120d6a1f99df4
Surya <surya@Laptop.local>
2025-11-27 21:58:50 +0700

    section 10

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -190,8 +190,11 @@
 
 	<!-- Section 5 -->
 	<div class="p-20 flex flex-col items-center">
-		<p class="text-4xl leading-15 font-semibold text-center">Confused about Choosing the Right Service for Your Career Center Needs? Don't Worry!</p>
-		<p class="text-lg text-center text-[#4B4B4B] my-5">In addition to supporting students individually, Career Support helps schools create a professional career center. All student and alumni data can be monitored and it makes it easier for them to connect to job and internship opportunities from partner companies.</p>
+		<p class="text-4xl leading-15 font-semibold text-center">Confused about Choosing the Right Service for Your
+			Career Center Needs? Don't Worry!</p>
+		<p class="text-lg text-center text-[#4B4B4B] my-5">In addition to supporting students individually, Career
+			Support helps schools create a professional career center. All student and alumni data can be monitored and
+			it makes it easier for them to connect to job and internship opportunities from partner companies.</p>
 
 		<div class="flex gap-x-5 w-full mt-5">
 			<div class="bg-white border border-[#E3E3E3] p-5 rounded-xl w-full flex flex-col items-center">
@@ -233,17 +236,20 @@
 	</div>
 
 	<!-- Section 6 -->
-	 <div class="p-20 flex items-center gap-x-10">
+	<div class="p-20 flex items-center gap-x-10">
 		<div class="w-[50%]">
 			<p class="text-4xl leading-15 font-semibold">Boost Your Skills & Prepare Your Career with Skillhub</p>
-			<p class="text-lg text-[#4B4B4B] my-5">Skillhub is an exclusive online course platform integrated into the Career Support App, helping students develop abilities relevant to the working world flexibly and effectively.</p>
+			<p class="text-lg text-[#4B4B4B] my-5">Skillhub is an exclusive online course platform integrated into the
+				Career Support App, helping students develop abilities relevant to the working world flexibly and
+				effectively.</p>
 
 			<button type="button" class="custom-button my-5">Learn More</button>
 
 			<div class="flex flex-col gap-y-6 mt-3">
 				<div class="custom-list-tile">
 					<div class="rounded-full w-3 h-3 bg-[#ED1966] mx-2"></div>
-					<span class="text-[#171717] text-xl">Material tailored to students' career needs, from soft skills to technical skills</span>
+					<span class="text-[#171717] text-xl">Material tailored to students' career needs, from soft skills
+						to technical skills</span>
 				</div>
 				<div class="custom-list-tile">
 					<div class="rounded-full w-3 h-3 bg-[#ED1966] mx-2"></div>
@@ -251,7 +257,8 @@
 				</div>
 				<div class="custom-list-tile">
 					<div class="rounded-full w-3 h-3 bg-[#ED1966] mx-2"></div>
-					<span class="text-[#171717] text-xl">Additional skills that make you more ready to face the working world</span>
+					<span class="text-[#171717] text-xl">Additional skills that make you more ready to face the working
+						world</span>
 				</div>
 				<div class="custom-list-tile">
 					<div class="rounded-full w-3 h-3 bg-[#ED1966] mx-2"></div>
@@ -259,42 +266,128 @@
 				</div>
 				<div class="custom-list-tile">
 					<div class="rounded-full w-3 h-3 bg-[#ED1966] mx-2"></div>
-					<span class="text-[#171717] text-xl">All skills and certificates are automatically recorded on the student's profile</span>
+					<span class="text-[#171717] text-xl">All skills and certificates are automatically recorded on the
+						student's profile</span>
 				</div>
 			</div>
 		</div>
 		<div class="w-[50%]">
 			<img src="@/assets/images/skillhub.png" alt="skillhub image" class="rounded-xl">
 		</div>
-	 </div>
+	</div>
 
-	 <!-- Section 9 -->
-	  <div class="p-20">
+	<!-- Section 9 -->
+	<div class="p-20">
 		<div class="pb-5">
 			<p class="text-4xl leading-15 font-semibold text-center">Institutional Partners</p>
-			<p class="text-lg text-[#4B4B4B] my-5 text-center">Institutions that have collaborated and are integrated with Career Support.</p>
-			
+			<p class="text-lg text-[#4B4B4B] my-5 text-center">Institutions that have collaborated and are integrated
+				with Career Support.</p>
+
 			<div class="flex items-center justify-evenly">
-				<img v-for="n in Array.from({ length: 8 }, (v, i) => i + 1)" :key="n" :src="getImage('institutional_partner', n)" :alt="`partner_img_${n}`" class="w-[150px] h-[150px]">
+				<img v-for="n in Array.from({ length: 8 }, (v, i) => i + 1)" :key="n"
+					:src="getImage('institutional_partner', n)" :alt="`partner_img_${n}`" class="w-[150px] h-[150px]">
 			</div>
 		</div>
 		<div class="py-5">
 			<p class="text-4xl leading-15 font-semibold text-center">School Partners</p>
-			<p class="text-lg text-[#4B4B4B] my-5 text-center">Schools that have actively partnered with Career Support to build a digital career center and monitor the development of students and alumni.</p>
-			
+			<p class="text-lg text-[#4B4B4B] my-5 text-center">Schools that have actively partnered with Career Support
+				to build a digital career center and monitor the development of students and alumni.</p>
+
 			<div class="flex items-center justify-evenly">
-				<img v-for="n in Array.from({ length: 7 }, (v, i) => i + 1)" :key="n" :src="getImage('school_partner', n)" :alt="`partner_img_${n}`" class="w-[150px] h-[150px]">
+				<img v-for="n in Array.from({ length: 7 }, (v, i) => i + 1)" :key="n"
+					:src="getImage('school_partner', n)" :alt="`partner_img_${n}`" class="w-[150px] h-[150px]">
 			</div>
 		</div>
 		<div>
 			<p class="text-4xl leading-15 font-semibold text-center">Company Partners</p>
-			<p class="text-lg text-[#4B4B4B] my-5 text-center">Companies that provide job vacancies and support career activities for students.</p>
-			
+			<p class="text-lg text-[#4B4B4B] my-5 text-center">Companies that provide job vacancies and support career
+				activities for students.</p>
+
 			<div class="flex items-center justify-evenly">
-				<img v-for="n in Array.from({ length: 7 }, (v, i) => i + 1)" :key="n" :src="getImage('company_partner', n)" :alt="`partner_img_${n}`" class="w-[150px] h-[150px]">
+				<img v-for="n in Array.from({ length: 7 }, (v, i) => i + 1)" :key="n"
+					:src="getImage('company_partner', n)" :alt="`partner_img_${n}`" class="w-[150px] h-[150px]">
 			</div>
 		</div>
-	  </div>
+	</div>
+
+	<!-- Section 10 -->
+	<div class="w-full bg-[#195699] p-20 flex flex-col items-center">
+		<p class="text-4xl leading-15 font-semibold text-white">Don't Miss Upcoming Events & Activities!</p>
+		<p class="text-lg text-[#4B4B4B] my-5 text-white">Career Support regularly organizes events to support students'
+			career development, ranging from workshops, webinars, to job fairs and skill training.</p>
+
+		<div class="flex items-center justify-center gap-x-5 mt-5">
+			<div class="custom-card">
+				<div class="w-[375px] h-[200px] bg-[#D9D9D9] rounded-xl"></div>
+
+				<p class="text-xl mt-5 mb-3">Job Fair 2026</p>
+				<div class="flex items-center text-[#818C9F] mb-3">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+						<rect width="24" height="24" fill="none" />
+						<path fill="currentColor" fill-rule="evenodd" d="M7 1.75a.75.75 0 0 1 .75.75v.763c.662-.013 1.391-.013 2.194-.013h4.112c.803 0 1.532 0 2.194.013V2.5a.75.75 0 0 1 1.5 0v.827q.39.03.739.076c1.172.158 2.121.49 2.87 1.238c.748.749 1.08 1.698 1.238 2.87q.074.562.107 1.23a.75.75 0 0 1 .019.46c.027.801.027 1.712.027 2.743v2.112c0 1.838 0 3.294-.153 4.433c-.158 1.172-.49 2.121-1.238 2.87c-.749.748-1.698 1.08-2.87 1.238c-1.14.153-2.595.153-4.433.153H9.944c-1.838 0-3.294 0-4.433-.153c-1.172-.158-2.121-.49-2.87-1.238c-.748-.749-1.08-1.698-1.238-2.87c-.153-1.14-.153-2.595-.153-4.433v-2.112c0-1.031 0-1.942.027-2.744a.75.75 0 0 1 .02-.46q.032-.667.106-1.229c.158-1.172.49-2.121 1.238-2.87c.749-.748 1.698-1.08 2.87-1.238q.35-.046.739-.076V2.5A.75.75 0 0 1 7 1.75m-4.237 8c-.013.653-.013 1.396-.013 2.25v2c0 1.907.002 3.262.14 4.29c.135 1.005.389 1.585.812 2.008s1.003.677 2.009.812c1.028.138 2.382.14 4.289.14h4c1.907 0 3.262-.002 4.29-.14c1.005-.135 1.585-.389 2.008-.812s.677-1.003.812-2.009c.138-1.027.14-2.382.14-4.289v-2c0-.854 0-1.597-.013-2.25zm18.405-1.5H2.832q.024-.284.058-.54c.135-1.005.389-1.585.812-2.008s1.003-.677 2.009-.812c1.028-.138 2.382-.14 4.289-.14h4c1.907 0 3.262.002 4.29.14c1.005.135 1.585.389 2.008.812s.677 1.003.812 2.009q.034.255.058.539" clip-rule="evenodd" />
+					</svg>
+					<span class="ml-2">Monday, 20 January 2026</span>
+				</div>
+				<div class="flex items-center text-[#818C9F] mb-3">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+						<rect width="24" height="24" fill="none" />
+						<path fill="currentColor" fill-rule="evenodd" d="M12 2.75a9.25 9.25 0 1 0 0 18.5a9.25 9.25 0 0 0 0-18.5M1.25 12C1.25 6.063 6.063 1.25 12 1.25S22.75 6.063 22.75 12S17.937 22.75 12 22.75S1.25 17.937 1.25 12M12 7.25a.75.75 0 0 1 .75.75v3.69l2.28 2.28a.75.75 0 1 1-1.06 1.06l-2.5-2.5a.75.75 0 0 1-.22-.53V8a.75.75 0 0 1 .75-.75" clip-rule="evenodd" />
+					</svg>
+					<span class="ml-2">10:00 - 11:00</span>
+				</div>
+
+				<button class="button sold-out mt-5">
+					<span>Sold Out</span>
+				</button>
+			</div>
+			<div class="custom-card">
+				<div class="w-[375px] h-[200px] bg-[#D9D9D9] rounded-xl"></div>
+
+				<p class="text-xl mt-5 mb-3">Webinar CV Klinik untuk Mahasiswa</p>
+				<div class="flex items-center text-[#818C9F] mb-3">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+						<rect width="24" height="24" fill="none" />
+						<path fill="currentColor" fill-rule="evenodd" d="M7 1.75a.75.75 0 0 1 .75.75v.763c.662-.013 1.391-.013 2.194-.013h4.112c.803 0 1.532 0 2.194.013V2.5a.75.75 0 0 1 1.5 0v.827q.39.03.739.076c1.172.158 2.121.49 2.87 1.238c.748.749 1.08 1.698 1.238 2.87q.074.562.107 1.23a.75.75 0 0 1 .019.46c.027.801.027 1.712.027 2.743v2.112c0 1.838 0 3.294-.153 4.433c-.158 1.172-.49 2.121-1.238 2.87c-.749.748-1.698 1.08-2.87 1.238c-1.14.153-2.595.153-4.433.153H9.944c-1.838 0-3.294 0-4.433-.153c-1.172-.158-2.121-.49-2.87-1.238c-.748-.749-1.08-1.698-1.238-2.87c-.153-1.14-.153-2.595-.153-4.433v-2.112c0-1.031 0-1.942.027-2.744a.75.75 0 0 1 .02-.46q.032-.667.106-1.229c.158-1.172.49-2.121 1.238-2.87c.749-.748 1.698-1.08 2.87-1.238q.35-.046.739-.076V2.5A.75.75 0 0 1 7 1.75m-4.237 8c-.013.653-.013 1.396-.013 2.25v2c0 1.907.002 3.262.14 4.29c.135 1.005.389 1.585.812 2.008s1.003.677 2.009.812c1.028.138 2.382.14 4.289.14h4c1.907 0 3.262-.002 4.29-.14c1.005-.135 1.585-.389 2.008-.812s.677-1.003.812-2.009c.138-1.027.14-2.382.14-4.289v-2c0-.854 0-1.597-.013-2.25zm18.405-1.5H2.832q.024-.284.058-.54c.135-1.005.389-1.585.812-2.008s1.003-.677 2.009-.812c1.028-.138 2.382-.14 4.289-.14h4c1.907 0 3.262.002 4.29.14c1.005.135 1.585.389 2.008.812s.677 1.003.812 2.009q.034.255.058.539" clip-rule="evenodd" />
+					</svg>
+					<span class="ml-2">Monday, 20 January 2026</span>
+				</div>
+				<div class="flex items-center text-[#818C9F] mb-3">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+						<rect width="24" height="24" fill="none" />
+						<path fill="currentColor" fill-rule="evenodd" d="M12 2.75a9.25 9.25 0 1 0 0 18.5a9.25 9.25 0 0 0 0-18.5M1.25 12C1.25 6.063 6.063 1.25 12 1.25S22.75 6.063 22.75 12S17.937 22.75 12 22.75S1.25 17.937 1.25 12M12 7.25a.75.75 0 0 1 .75.75v3.69l2.28 2.28a.75.75 0 1 1-1.06 1.06l-2.5-2.5a.75.75 0 0 1-.22-.53V8a.75.75 0 0 1 .75-.75" clip-rule="evenodd" />
+					</svg>
+					<span class="ml-2">10:00 - 11:00</span>
+				</div>
+
+				<button class="button join mt-5">
+					<span>Join</span>
+				</button>
+			</div>
+			<div class="custom-card">
+				<div class="w-[375px] h-[200px] bg-[#D9D9D9] rounded-xl"></div>
+
+				<p class="text-xl mt-5 mb-3">Webinar 01: Tips interview agar dilirik HR</p>
+				<div class="flex items-center text-[#818C9F] mb-3">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+						<rect width="24" height="24" fill="none" />
+						<path fill="currentColor" fill-rule="evenodd" d="M7 1.75a.75.75 0 0 1 .75.75v.763c.662-.013 1.391-.013 2.194-.013h4.112c.803 0 1.532 0 2.194.013V2.5a.75.75 0 0 1 1.5 0v.827q.39.03.739.076c1.172.158 2.121.49 2.87 1.238c.748.749 1.08 1.698 1.238 2.87q.074.562.107 1.23a.75.75 0 0 1 .019.46c.027.801.027 1.712.027 2.743v2.112c0 1.838 0 3.294-.153 4.433c-.158 1.172-.49 2.121-1.238 2.87c-.749.748-1.698 1.08-2.87 1.238c-1.14.153-2.595.153-4.433.153H9.944c-1.838 0-3.294 0-4.433-.153c-1.172-.158-2.121-.49-2.87-1.238c-.748-.749-1.08-1.698-1.238-2.87c-.153-1.14-.153-2.595-.153-4.433v-2.112c0-1.031 0-1.942.027-2.744a.75.75 0 0 1 .02-.46q.032-.667.106-1.229c.158-1.172.49-2.121 1.238-2.87c.749-.748 1.698-1.08 2.87-1.238q.35-.046.739-.076V2.5A.75.75 0 0 1 7 1.75m-4.237 8c-.013.653-.013 1.396-.013 2.25v2c0 1.907.002 3.262.14 4.29c.135 1.005.389 1.585.812 2.008s1.003.677 2.009.812c1.028.138 2.382.14 4.289.14h4c1.907 0 3.262-.002 4.29-.14c1.005-.135 1.585-.389 2.008-.812s.677-1.003.812-2.009c.138-1.027.14-2.382.14-4.289v-2c0-.854 0-1.597-.013-2.25zm18.405-1.5H2.832q.024-.284.058-.54c.135-1.005.389-1.585.812-2.008s1.003-.677 2.009-.812c1.028-.138 2.382-.14 4.289-.14h4c1.907 0 3.262.002 4.29.14c1.005.135 1.585.389 2.008.812s.677 1.003.812 2.009q.034.255.058.539" clip-rule="evenodd" />
+					</svg>
+					<span class="ml-2">Monday, 20 January 2026</span>
+				</div>
+				<div class="flex items-center text-[#818C9F] mb-3">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+						<rect width="24" height="24" fill="none" />
+						<path fill="currentColor" fill-rule="evenodd" d="M12 2.75a9.25 9.25 0 1 0 0 18.5a9.25 9.25 0 0 0 0-18.5M1.25 12C1.25 6.063 6.063 1.25 12 1.25S22.75 6.063 22.75 12S17.937 22.75 12 22.75S1.25 17.937 1.25 12M12 7.25a.75.75 0 0 1 .75.75v3.69l2.28 2.28a.75.75 0 1 1-1.06 1.06l-2.5-2.5a.75.75 0 0 1-.22-.53V8a.75.75 0 0 1 .75-.75" clip-rule="evenodd" />
+					</svg>
+					<span class="ml-2">10:00 - 11:00</span>
+				</div>
+
+				<button class="button join mt-5">
+					<span>Join</span>
+				</button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts" setup>
@@ -329,5 +422,39 @@ const getImage = (path: string, n: number) => {
 	display: flex;
 	align-items: center;
 	gap: 1rem;
+}
+
+.custom-card {
+	width: '500px';
+	background-color: white;
+	padding: 1rem;
+	border-radius: 1.25rem;
+}
+
+.custom-card .button {
+	width: 100%;
+	border-radius: 1rem;
+
+	padding: 0.5rem 1rem;
+	cursor: pointer;
+	transition: background-color 0.3s ease;
+}
+
+.custom-card .button.sold-out {
+	border: 1px solid #D8E4ED;
+	background-color: white;
+}
+
+.custom-card .button.sold-out:hover {
+	background-color: #F0F4F8;
+}
+
+.custom-card .button.join {
+	background-color: #4682C4;
+	color: white;
+}
+
+.custom-card .button.join:hover {
+	background-color: #356899;
 }
 </style>
