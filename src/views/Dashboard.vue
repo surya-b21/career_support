45bcdd5970dc86dd6d977920e1b9c60fb17a94f5
Surya <surya@Laptop.local>
2025-12-14 22:17:25 +0700

    update section 7

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -261,9 +261,10 @@
 					<path fill="currentColor" fill-rule="evenodd" d="M15.488 4.43a.75.75 0 0 1 .081 1.058L9.988 12l5.581 6.512a.75.75 0 1 1-1.138.976l-6-7a.75.75 0 0 1 0-.976l6-7a.75.75 0 0 1 1.057-.081" clip-rule="evenodd" />
 				</svg>
 			</button>
-			<div class="flex items-center justify-center gap-x-5 mt-5">
-				<!-- Mobile: show only 1 card -->
-				<div class="custom-card w-[250px] h-[265px] md:hidden" :key="currentSlide">
+			
+			<!-- Mobile: show only 1 card -->
+			<div class="flex items-center justify-center mt-5 md:hidden">
+				<div class="custom-card w-[250px] h-[265px]" :key="currentSlide">
 					<img class="image-container" :src="section7CardsContent[currentSlide]?.img" :alt="section7CardsContent[currentSlide]?.title" />
 
 					<div class="flex items-center text-[#818C9F] my-2 md:my-5">
@@ -276,9 +277,11 @@
 
 					<p class="font-medium text-[#08151E]">{{ section7CardsContent[currentSlide]?.title }}</p>
 				</div>
+			</div>
 
-				<!-- Desktop: show 3 cards -->
-				<div class="custom-card w-[350px] h-[352px] hidden md:block" v-for="index in [0, 1, 2]" :key="currentSlide + index">
+			<!-- Desktop: show 3 cards -->
+			<div class="hidden md:flex items-center justify-center gap-x-5 mt-5">
+				<div class="custom-card w-[350px] h-[352px]" v-for="index in [0, 1, 2]" :key="currentSlide + index">
 					<img class="image-container" :src="section7CardsContent[currentSlide + index]?.img" :alt="section7CardsContent[currentSlide + index]?.title" />
 
 					<div class="flex items-center text-[#818C9F] my-2 md:my-5">
@@ -292,6 +295,7 @@
 					<p class="text-lg font-medium text-[#08151E]">{{ section7CardsContent[currentSlide + index]?.title }}</p>
 				</div>
 			</div>
+
 			<button class="arrow-button" @click="nextSlide">
 				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
 					<rect width="24" height="24" fill="none" />
@@ -384,19 +388,21 @@
 				<div class="image-container bg-[#D9D9D9]"></div>
 
 				<p class="text-xl mt-5 mb-3">Job Fair 2026</p>
-				<div class="flex items-center text-[#818C9F] mb-3">
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
-						<rect width="24" height="24" fill="none" />
-						<path fill="currentColor" fill-rule="evenodd" d="M7 1.75a.75.75 0 0 1 .75.75v.763c.662-.013 1.391-.013 2.194-.013h4.112c.803 0 1.532 0 2.194.013V2.5a.75.75 0 0 1 1.5 0v.827q.39.03.739.076c1.172.158 2.121.49 2.87 1.238c.748.749 1.08 1.698 1.238 2.87q.074.562.107 1.23a.75.75 0 0 1 .019.46c.027.801.027 1.712.027 2.743v2.112c0 1.838 0 3.294-.153 4.433c-.158 1.172-.49 2.121-1.238 2.87c-.749.748-1.698 1.08-2.87 1.238c-1.14.153-2.595.153-4.433.153H9.944c-1.838 0-3.294 0-4.433-.153c-1.172-.158-2.121-.49-2.87-1.238c-.748-.749-1.08-1.698-1.238-2.87c-.153-1.14-.153-2.595-.153-4.433v-2.112c0-1.031 0-1.942.027-2.744a.75.75 0 0 1 .02-.46q.032-.667.106-1.229c.158-1.172.49-2.121 1.238-2.87c.749-.748 1.698-1.08 2.87-1.238q.35-.046.739-.076V2.5A.75.75 0 0 1 7 1.75m-4.237 8c-.013.653-.013 1.396-.013 2.25v2c0 1.907.002 3.262.14 4.29c.135 1.005.389 1.585.812 2.008s1.003.677 2.009.812c1.028.138 2.382.14 4.289.14h4c1.907 0 3.262-.002 4.29-.14c1.005-.135 1.585-.389 2.008-.812s.677-1.003.812-2.009c.138-1.027.14-2.382.14-4.289v-2c0-.854 0-1.597-.013-2.25zm18.405-1.5H2.832q.024-.284.058-.54c.135-1.005.389-1.585.812-2.008s1.003-.677 2.009-.812c1.028-.138 2.382-.14 4.289-.14h4c1.907 0 3.262.002 4.29.14c1.005.135 1.585.389 2.008.812s.677 1.003.812 2.009q.034.255.058.539" clip-rule="evenodd" />
-					</svg>
-					<span class="ml-2">Monday, 20 January 2026</span>
-				</div>
-				<div class="flex items-center text-[#818C9F] mb-3">
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
-						<rect width="24" height="24" fill="none" />
-						<path fill="currentColor" fill-rule="evenodd" d="M12 2.75a9.25 9.25 0 1 0 0 18.5a9.25 9.25 0 0 0 0-18.5M1.25 12C1.25 6.063 6.063 1.25 12 1.25S22.75 6.063 22.75 12S17.937 22.75 12 22.75S1.25 17.937 1.25 12M12 7.25a.75.75 0 0 1 .75.75v3.69l2.28 2.28a.75.75 0 1 1-1.06 1.06l-2.5-2.5a.75.75 0 0 1-.22-.53V8a.75.75 0 0 1 .75-.75" clip-rule="evenodd" />
-					</svg>
-					<span class="ml-2">10:00 - 11:00</span>
+				<div>
+					<div class="flex items-center text-[#818C9F] mb-3">
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+							<rect width="24" height="24" fill="none" />
+							<path fill="currentColor" fill-rule="evenodd" d="M7 1.75a.75.75 0 0 1 .75.75v.763c.662-.013 1.391-.013 2.194-.013h4.112c.803 0 1.532 0 2.194.013V2.5a.75.75 0 0 1 1.5 0v.827q.39.03.739.076c1.172.158 2.121.49 2.87 1.238c.748.749 1.08 1.698 1.238 2.87q.074.562.107 1.23a.75.75 0 0 1 .019.46c.027.801.027 1.712.027 2.743v2.112c0 1.838 0 3.294-.153 4.433c-.158 1.172-.49 2.121-1.238 2.87c-.749.748-1.698 1.08-2.87 1.238c-1.14.153-2.595.153-4.433.153H9.944c-1.838 0-3.294 0-4.433-.153c-1.172-.158-2.121-.49-2.87-1.238c-.748-.749-1.08-1.698-1.238-2.87c-.153-1.14-.153-2.595-.153-4.433v-2.112c0-1.031 0-1.942.027-2.744a.75.75 0 0 1 .02-.46q.032-.667.106-1.229c.158-1.172.49-2.121 1.238-2.87c.749-.748 1.698-1.08 2.87-1.238q.35-.046.739-.076V2.5A.75.75 0 0 1 7 1.75m-4.237 8c-.013.653-.013 1.396-.013 2.25v2c0 1.907.002 3.262.14 4.29c.135 1.005.389 1.585.812 2.008s1.003.677 2.009.812c1.028.138 2.382.14 4.289.14h4c1.907 0 3.262-.002 4.29-.14c1.005-.135 1.585-.389 2.008-.812s.677-1.003.812-2.009c.138-1.027.14-2.382.14-4.289v-2c0-.854 0-1.597-.013-2.25zm18.405-1.5H2.832q.024-.284.058-.54c.135-1.005.389-1.585.812-2.008s1.003-.677 2.009-.812c1.028-.138 2.382-.14 4.289-.14h4c1.907 0 3.262.002 4.29.14c1.005.135 1.585.389 2.008.812s.677 1.003.812 2.009q.034.255.058.539" clip-rule="evenodd" />
+						</svg>
+						<span class="ml-2">Monday, 20 January 2026</span>
+					</div>
+					<div class="flex items-center text-[#818C9F] mb-3">
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+							<rect width="24" height="24" fill="none" />
+							<path fill="currentColor" fill-rule="evenodd" d="M12 2.75a9.25 9.25 0 1 0 0 18.5a9.25 9.25 0 0 0 0-18.5M1.25 12C1.25 6.063 6.063 1.25 12 1.25S22.75 6.063 22.75 12S17.937 22.75 12 22.75S1.25 17.937 1.25 12M12 7.25a.75.75 0 0 1 .75.75v3.69l2.28 2.28a.75.75 0 1 1-1.06 1.06l-2.5-2.5a.75.75 0 0 1-.22-.53V8a.75.75 0 0 1 .75-.75" clip-rule="evenodd" />
+						</svg>
+						<span class="ml-2">10:00 - 11:00</span>
+					</div>
 				</div>
 
 				<button class="button sold-out mt-5">
@@ -407,19 +413,21 @@
 				<div class="image-container bg-[#D9D9D9]"></div>
 
 				<p class="text-xl mt-5 mb-3">Webinar CV Klinik untuk Mahasiswa</p>
-				<div class="flex items-center text-[#818C9F] mb-3">
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
-						<rect width="24" height="24" fill="none" />
-						<path fill="currentColor" fill-rule="evenodd" d="M7 1.75a.75.75 0 0 1 .75.75v.763c.662-.013 1.391-.013 2.194-.013h4.112c.803 0 1.532 0 2.194.013V2.5a.75.75 0 0 1 1.5 0v.827q.39.03.739.076c1.172.158 2.121.49 2.87 1.238c.748.749 1.08 1.698 1.238 2.87q.074.562.107 1.23a.75.75 0 0 1 .019.46c.027.801.027 1.712.027 2.743v2.112c0 1.838 0 3.294-.153 4.433c-.158 1.172-.49 2.121-1.238 2.87c-.749.748-1.698 1.08-2.87 1.238c-1.14.153-2.595.153-4.433.153H9.944c-1.838 0-3.294 0-4.433-.153c-1.172-.158-2.121-.49-2.87-1.238c-.748-.749-1.08-1.698-1.238-2.87c-.153-1.14-.153-2.595-.153-4.433v-2.112c0-1.031 0-1.942.027-2.744a.75.75 0 0 1 .02-.46q.032-.667.106-1.229c.158-1.172.49-2.121 1.238-2.87c.749-.748 1.698-1.08 2.87-1.238q.35-.046.739-.076V2.5A.75.75 0 0 1 7 1.75m-4.237 8c-.013.653-.013 1.396-.013 2.25v2c0 1.907.002 3.262.14 4.29c.135 1.005.389 1.585.812 2.008s1.003.677 2.009.812c1.028.138 2.382.14 4.289.14h4c1.907 0 3.262-.002 4.29-.14c1.005-.135 1.585-.389 2.008-.812s.677-1.003.812-2.009c.138-1.027.14-2.382.14-4.289v-2c0-.854 0-1.597-.013-2.25zm18.405-1.5H2.832q.024-.284.058-.54c.135-1.005.389-1.585.812-2.008s1.003-.677 2.009-.812c1.028-.138 2.382-.14 4.289-.14h4c1.907 0 3.262.002 4.29.14c1.005.135 1.585.389 2.008.812s.677 1.003.812 2.009q.034.255.058.539" clip-rule="evenodd" />
-					</svg>
-					<span class="ml-2">Monday, 20 January 2026</span>
-				</div>
-				<div class="flex items-center text-[#818C9F] mb-3">
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
-						<rect width="24" height="24" fill="none" />
-						<path fill="currentColor" fill-rule="evenodd" d="M12 2.75a9.25 9.25 0 1 0 0 18.5a9.25 9.25 0 0 0 0-18.5M1.25 12C1.25 6.063 6.063 1.25 12 1.25S22.75 6.063 22.75 12S17.937 22.75 12 22.75S1.25 17.937 1.25 12M12 7.25a.75.75 0 0 1 .75.75v3.69l2.28 2.28a.75.75 0 1 1-1.06 1.06l-2.5-2.5a.75.75 0 0 1-.22-.53V8a.75.75 0 0 1 .75-.75" clip-rule="evenodd" />
-					</svg>
-					<span class="ml-2">10:00 - 11:00</span>
+				<div>
+					<div class="flex items-center text-[#818C9F] mb-3">
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+							<rect width="24" height="24" fill="none" />
+							<path fill="currentColor" fill-rule="evenodd" d="M7 1.75a.75.75 0 0 1 .75.75v.763c.662-.013 1.391-.013 2.194-.013h4.112c.803 0 1.532 0 2.194.013V2.5a.75.75 0 0 1 1.5 0v.827q.39.03.739.076c1.172.158 2.121.49 2.87 1.238c.748.749 1.08 1.698 1.238 2.87q.074.562.107 1.23a.75.75 0 0 1 .019.46c.027.801.027 1.712.027 2.743v2.112c0 1.838 0 3.294-.153 4.433c-.158 1.172-.49 2.121-1.238 2.87c-.749.748-1.698 1.08-2.87 1.238c-1.14.153-2.595.153-4.433.153H9.944c-1.838 0-3.294 0-4.433-.153c-1.172-.158-2.121-.49-2.87-1.238c-.748-.749-1.08-1.698-1.238-2.87c-.153-1.14-.153-2.595-.153-4.433v-2.112c0-1.031 0-1.942.027-2.744a.75.75 0 0 1 .02-.46q.032-.667.106-1.229c.158-1.172.49-2.121 1.238-2.87c.749-.748 1.698-1.08 2.87-1.238q.35-.046.739-.076V2.5A.75.75 0 0 1 7 1.75m-4.237 8c-.013.653-.013 1.396-.013 2.25v2c0 1.907.002 3.262.14 4.29c.135 1.005.389 1.585.812 2.008s1.003.677 2.009.812c1.028.138 2.382.14 4.289.14h4c1.907 0 3.262-.002 4.29-.14c1.005-.135 1.585-.389 2.008-.812s.677-1.003.812-2.009c.138-1.027.14-2.382.14-4.289v-2c0-.854 0-1.597-.013-2.25zm18.405-1.5H2.832q.024-.284.058-.54c.135-1.005.389-1.585.812-2.008s1.003-.677 2.009-.812c1.028-.138 2.382-.14 4.289-.14h4c1.907 0 3.262.002 4.29.14c1.005.135 1.585.389 2.008.812s.677 1.003.812 2.009q.034.255.058.539" clip-rule="evenodd" />
+						</svg>
+						<span class="ml-2">Monday, 20 January 2026</span>
+					</div>
+					<div class="flex items-center text-[#818C9F] mb-3">
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+							<rect width="24" height="24" fill="none" />
+							<path fill="currentColor" fill-rule="evenodd" d="M12 2.75a9.25 9.25 0 1 0 0 18.5a9.25 9.25 0 0 0 0-18.5M1.25 12C1.25 6.063 6.063 1.25 12 1.25S22.75 6.063 22.75 12S17.937 22.75 12 22.75S1.25 17.937 1.25 12M12 7.25a.75.75 0 0 1 .75.75v3.69l2.28 2.28a.75.75 0 1 1-1.06 1.06l-2.5-2.5a.75.75 0 0 1-.22-.53V8a.75.75 0 0 1 .75-.75" clip-rule="evenodd" />
+						</svg>
+						<span class="ml-2">10:00 - 11:00</span>
+					</div>
 				</div>
 
 				<button class="button join mt-5">
@@ -430,19 +438,21 @@
 				<div class="image-container bg-[#D9D9D9]"></div>
 
 				<p class="text-xl mt-5 mb-3">Webinar 01: Tips interview agar dilirik HR</p>
-				<div class="flex items-center text-[#818C9F] mb-3">
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
-						<rect width="24" height="24" fill="none" />
-						<path fill="currentColor" fill-rule="evenodd" d="M7 1.75a.75.75 0 0 1 .75.75v.763c.662-.013 1.391-.013 2.194-.013h4.112c.803 0 1.532 0 2.194.013V2.5a.75.75 0 0 1 1.5 0v.827q.39.03.739.076c1.172.158 2.121.49 2.87 1.238c.748.749 1.08 1.698 1.238 2.87q.074.562.107 1.23a.75.75 0 0 1 .019.46c.027.801.027 1.712.027 2.743v2.112c0 1.838 0 3.294-.153 4.433c-.158 1.172-.49 2.121-1.238 2.87c-.749.748-1.698 1.08-2.87 1.238c-1.14.153-2.595.153-4.433.153H9.944c-1.838 0-3.294 0-4.433-.153c-1.172-.158-2.121-.49-2.87-1.238c-.748-.749-1.08-1.698-1.238-2.87c-.153-1.14-.153-2.595-.153-4.433v-2.112c0-1.031 0-1.942.027-2.744a.75.75 0 0 1 .02-.46q.032-.667.106-1.229c.158-1.172.49-2.121 1.238-2.87c.749-.748 1.698-1.08 2.87-1.238q.35-.046.739-.076V2.5A.75.75 0 0 1 7 1.75m-4.237 8c-.013.653-.013 1.396-.013 2.25v2c0 1.907.002 3.262.14 4.29c.135 1.005.389 1.585.812 2.008s1.003.677 2.009.812c1.028.138 2.382.14 4.289.14h4c1.907 0 3.262-.002 4.29-.14c1.005-.135 1.585-.389 2.008-.812s.677-1.003.812-2.009c.138-1.027.14-2.382.14-4.289v-2c0-.854 0-1.597-.013-2.25zm18.405-1.5H2.832q.024-.284.058-.54c.135-1.005.389-1.585.812-2.008s1.003-.677 2.009-.812c1.028-.138 2.382-.14 4.289-.14h4c1.907 0 3.262.002 4.29.14c1.005.135 1.585.389 2.008.812s.677 1.003.812 2.009q.034.255.058.539" clip-rule="evenodd" />
-					</svg>
-					<span class="ml-2">Monday, 20 January 2026</span>
-				</div>
-				<div class="flex items-center text-[#818C9F] mb-3">
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
-						<rect width="24" height="24" fill="none" />
-						<path fill="currentColor" fill-rule="evenodd" d="M12 2.75a9.25 9.25 0 1 0 0 18.5a9.25 9.25 0 0 0 0-18.5M1.25 12C1.25 6.063 6.063 1.25 12 1.25S22.75 6.063 22.75 12S17.937 22.75 12 22.75S1.25 17.937 1.25 12M12 7.25a.75.75 0 0 1 .75.75v3.69l2.28 2.28a.75.75 0 1 1-1.06 1.06l-2.5-2.5a.75.75 0 0 1-.22-.53V8a.75.75 0 0 1 .75-.75" clip-rule="evenodd" />
-					</svg>
-					<span class="ml-2">10:00 - 11:00</span>
+				<div>
+					<div class="flex items-center text-[#818C9F] mb-3">
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+							<rect width="24" height="24" fill="none" />
+							<path fill="currentColor" fill-rule="evenodd" d="M7 1.75a.75.75 0 0 1 .75.75v.763c.662-.013 1.391-.013 2.194-.013h4.112c.803 0 1.532 0 2.194.013V2.5a.75.75 0 0 1 1.5 0v.827q.39.03.739.076c1.172.158 2.121.49 2.87 1.238c.748.749 1.08 1.698 1.238 2.87q.074.562.107 1.23a.75.75 0 0 1 .019.46c.027.801.027 1.712.027 2.743v2.112c0 1.838 0 3.294-.153 4.433c-.158 1.172-.49 2.121-1.238 2.87c-.749.748-1.698 1.08-2.87 1.238c-1.14.153-2.595.153-4.433.153H9.944c-1.838 0-3.294 0-4.433-.153c-1.172-.158-2.121-.49-2.87-1.238c-.748-.749-1.08-1.698-1.238-2.87c-.153-1.14-.153-2.595-.153-4.433v-2.112c0-1.031 0-1.942.027-2.744a.75.75 0 0 1 .02-.46q.032-.667.106-1.229c.158-1.172.49-2.121 1.238-2.87c.749-.748 1.698-1.08 2.87-1.238q.35-.046.739-.076V2.5A.75.75 0 0 1 7 1.75m-4.237 8c-.013.653-.013 1.396-.013 2.25v2c0 1.907.002 3.262.14 4.29c.135 1.005.389 1.585.812 2.008s1.003.677 2.009.812c1.028.138 2.382.14 4.289.14h4c1.907 0 3.262-.002 4.29-.14c1.005-.135 1.585-.389 2.008-.812s.677-1.003.812-2.009c.138-1.027.14-2.382.14-4.289v-2c0-.854 0-1.597-.013-2.25zm18.405-1.5H2.832q.024-.284.058-.54c.135-1.005.389-1.585.812-2.008s1.003-.677 2.009-.812c1.028-.138 2.382-.14 4.289-.14h4c1.907 0 3.262.002 4.29.14c1.005.135 1.585.389 2.008.812s.677 1.003.812 2.009q.034.255.058.539" clip-rule="evenodd" />
+						</svg>
+						<span class="ml-2">Monday, 20 January 2026</span>
+					</div>
+					<div class="flex items-center text-[#818C9F] mb-3">
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+							<rect width="24" height="24" fill="none" />
+							<path fill="currentColor" fill-rule="evenodd" d="M12 2.75a9.25 9.25 0 1 0 0 18.5a9.25 9.25 0 0 0 0-18.5M1.25 12C1.25 6.063 6.063 1.25 12 1.25S22.75 6.063 22.75 12S17.937 22.75 12 22.75S1.25 17.937 1.25 12M12 7.25a.75.75 0 0 1 .75.75v3.69l2.28 2.28a.75.75 0 1 1-1.06 1.06l-2.5-2.5a.75.75 0 0 1-.22-.53V8a.75.75 0 0 1 .75-.75" clip-rule="evenodd" />
+						</svg>
+						<span class="ml-2">10:00 - 11:00</span>
+					</div>
 				</div>
 
 				<button class="button join mt-5">
@@ -529,14 +539,14 @@ const previousSection8Slide = () => {
 }
 
 const section7CardsContent = reactive([
-	{"title": "SMK YAJ Depok - Job Fair 2024", "img": new URL(`../assets/images/section7/1.png`, import.meta.url).href},
-	{"title": "SMKN 4 Jakarta - Job & Edu Fair 2024", "img": new URL(`../assets/images/section7/2.png`, import.meta.url).href},
-	{"title": "SMKN 33 Jakarta - Exploring Tourism Careers", "img": new URL(`../assets/images/section7/3.png`, import.meta.url).href},
-	{"title": "SMKN 69 Jakarta - Job Fair Membara 2024", "img": new URL(`../assets/images/section7/4.png`, import.meta.url).href},
-	{"title": "SMKS Assalam Depok - Bursa Kerja 2024", "img": new URL(`../assets/images/section7/5.png`, import.meta.url).href},
-	{"title": "Universitas Majalengka - UNMA Career Expo 2024", "img": new URL(`../assets/images/section7/6.png`, import.meta.url).href},
-	{"title": "SMK Islam PB Soedirman 2 - Career Day 2024", "img": new URL(`../assets/images/section7/7.png`, import.meta.url).href},
-	{"title": "SMK Telkom Bandung - Job Fair 2024", "img": new URL(`../assets/images/section7/8.png`, import.meta.url).href},
+	{"title": "SMK YAJ Depok - Job Fair 2024", "img": new URL(`../assets/images/section7/8.png`, import.meta.url).href},
+	{"title": "SMKN 4 Jakarta - Job & Edu Fair 2024", "img": new URL(`../assets/images/section7/1.png`, import.meta.url).href},
+	{"title": "SMKN 33 Jakarta - Exploring Tourism Careers", "img": new URL(`../assets/images/section7/2.png`, import.meta.url).href},
+	{"title": "SMKN 69 Jakarta - Job Fair Membara 2024", "img": new URL(`../assets/images/section7/3.png`, import.meta.url).href},
+	{"title": "SMKS Assalam Depok - Bursa Kerja 2024", "img": new URL(`../assets/images/section7/4.png`, import.meta.url).href},
+	{"title": "Universitas Majalengka - UNMA Career Expo 2024", "img": new URL(`../assets/images/section7/5.png`, import.meta.url).href},
+	{"title": "SMK Islam PB Soedirman 2 - Career Day 2024", "img": new URL(`../assets/images/section7/6.png`, import.meta.url).href},
+	{"title": "SMK Telkom Bandung - Job Fair 2024", "img": new URL(`../assets/images/section7/7.png`, import.meta.url).href},
 ])
 
 const section8CardsContent = reactive({
@@ -594,6 +604,9 @@ const section8CardsContent = reactive({
 	background-color: white;
 	padding: 1rem;
 	border-radius: 1.25rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
 }
 
 .custom-card .image-container {
